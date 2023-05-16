60531e87ac42a905058552468825914c74c411e0
Aiza Tahir <aizatahir2001@gmail.com>
2023-05-16 01:06:11 -0400

    updated code from Troeger

--- Graph_Project/Basic Data Types/stack.scm
+++ Graph_Project/Basic Data Types/stack.scm
@@ -5,42 +5,194 @@
 
 ; This is provided by the professor:
 
-(define (make-empty-stack)
-  (lambda (x)
-    'empty))
+
+;; Here is a way to have multiple representations for the same ADT (or
+;; interface) without resorting to the name-space clutter displayed in
+;; stackADT.scm
+
+;; One can add as many representations as one wishes inside the
+;; function stack-reps
+
+;; Status: DRAFT
 
 
-;; m is either 'top or 'pop
-;; s is a stack
-(define (push x s)
-  (lambda (m)
-    (cond ((eq? m 'top) x)
-          ((eq? m 'pop) s))))
 
-;; s is a stack
-(define (top s)
-  (s 'top))
+;; m is one of 'proc or 'list
 
-;; s is a non-empty stack
-(define (pop s)
-  (s 'pop))
+(define (stack-reps m)
+
+  (define (procedural-rep)
+    
+    (define (make-empty)
+      (lambda (m)
+        (eq? m 'empty?)))
+
+    ;; m is either 'top or 'pop
+    ;; s is a stack
+    (define (push x s)
+      (lambda (m)
+        (cond ((eq? m 'top) x)
+              ((eq? m 'pop) s)
+              ((eq? m 'empty?) #f))))
+
+    ;; s is a non-empty stack
+    (define (top s)
+      (s 'top))
+
+    ;; s is a non-empty stack
+    (define (pop s)
+      (s 'pop))
+
+    ;; s is a stack
+    (define (empty? s)
+      (s 'empty?))
+
+;; m is one of 'make-empty, 'push, 'top, 'pop, or 'empty?
+(define (dispatch m)
+  (cond ((eq? m 'make-empty) (make-empty))
+        ((eq? m 'push) push)
+        ((eq? m 'top) top)
+        ((eq? m 'pop) pop)
+        ((eq? m 'empty?) empty?)))
+
+    dispatch)
 
 
-;; do you see how to use these?
 
-;; first, set up some stacks 
+(define (list-rep)
 
-(define stack-0 (make-empty-stack))
+  (define make-empty
+    '())
 
-(define stack-1 (push 'a stack-0))
+  (define (push x s)
+    (cons x s))
 
-(define stack-2 (push 'b stack-1))
+  (define (top s)
+    (car s))
 
-;; now some computations
+  (define (pop s)
+    (cdr s))
 
-(define stack-3 (push 'c (pop stack-2)))
+  (define (empty? s)
+    (null? s))
 
-(list (top stack-3) (top stack-2) (top stack-1))
+  ;; m is one of 'make-empty, 'push, 'top, 'pop, or 'empty?
+  (define (dispatch m)
+    (cond ((eq? m 'make-empty) make-empty)
+          ((eq? m 'push) push)
+          ((eq? m 'top) top)
+          ((eq? m 'pop) pop)
+          ((eq? m 'empty?) empty?)))
+
+  dispatch)
 
 
-;; Add the rest of the stack implementations
+  
+
+  (cond ((eq? m 'proc) (procedural-rep))
+        ((eq? m 'list) (list-rep)))
+
+  )
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; usage -- comment out the list stacks section to experiment with
+;; procedural stacks -- comment out the procedural stacks section to
+;; experiment with list stacks.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; observe how the function stack-reps works to avoid cluttering the
+;; namespace with different versions of (for example) push: we no longer
+;; need disambiguation via names such as push-procedural and push-list
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;;; procedural stacks  -- comment out the list stacks section to
+;;; experiment with the procedural representation
+
+;;; first define the basic operations at the top level
+
+(define make-empty-stack ((stack-reps 'proc) 'make-empty))
+(define push-stack ((stack-reps 'proc) 'push))
+(define top-stack ((stack-reps 'proc) 'top))
+(define pop-stack ((stack-reps 'proc) 'pop))
+(define empty-stack? ((stack-reps 'proc) 'empty?))
+
+;;; now define some stacks
+
+(define stack-0 make-empty-stack)
+
+(define stack-1 (push-stack 'a stack-0))
+
+(define stack-2 (push-stack 'b stack-1))
+
+(define stack-3 (push-stack 'c (pop-stack stack-2)))
+
+
+;;; and now use them
+
+(list (top-stack stack-3) (top-stack stack-2) (top-stack stack-1))
+
+(define (demo-proc)  
+  (newline)
+  (display (top-stack stack-1))
+  (newline)
+  (display (empty-stack? (pop-stack stack-1)))
+  (newline)
+  (display (top-stack stack-1))
+  (newline))
+
+
+;; list stacks
+
+;;; first define the basic operations at the top level
+
+(define make-empty-stack ((stack-reps 'list) 'make-empty))
+(define push-stack ((stack-reps 'list) 'push))
+(define top-stack ((stack-reps 'list) 'top))
+(define pop-stack ((stack-reps 'list) 'pop))
+(define empty-stack? ((stack-reps 'list) 'empty?))
+
+;;; now define some stacks
+
+(define stack-0 make-empty-stack)
+
+(define stack-1 (push-stack 'a stack-0))
+
+(define stack-2 (push-stack 'b stack-1))
+
+(define stack-3 (push-stack 'c (pop-stack stack-2)))
+
+
+;;; and now use them
+
+(list (top-stack stack-3) (top-stack stack-2) (top-stack stack-1))
+
+(define (demo-list)  
+  (newline)
+  (display (top-stack stack-1))
+  (newline)
+  (display (empty-stack? (pop-stack stack-1)))
+  (newline)
+  (display (top-stack stack-1))
+  (newline))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Notice that this approach allows us to have stacks represented
+;; procedurally along side stacks represented as lists.  For example,
+
+(define stack-4 ((stack-reps 'proc) 'make-empty))
+
+(define stack-5 ((stack-reps 'list) 'make-empty))
+
+;; and then
+
+(((stack-reps 'proc) 'empty?) stack-4)
+
+(newline)
+
+(((stack-reps 'list) 'empty?) stack-5)
+    
