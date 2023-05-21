010de66a4067549e18840d79e6df251536a8b3c6
Aiza Tahir <79121131+aizatahir@users.noreply.github.com>
2023-05-20 23:38:28 -0400

    Update dijkstra.scm

--- Graph_Project/Graph_Computations/dijkstra.scm
+++ Graph_Project/Graph_Computations/dijkstra.scm
@@ -3,4 +3,8 @@
 (define graph (make-graph '(A B C D E)
                           '((A B) (A C) (B D) (B E) (C E) (D E))))
 
-(dijkstra graph 'A 'E) ; 6
+(dijkstra graph 'A 'E)
+
+
+; Testing this function gives an error which we didn't have time to fix, but it was supposed to return the shortest path
+; between the two given vertices. 
